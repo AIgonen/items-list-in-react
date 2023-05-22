4c377039b85f664c581259f59138717da6c380fa
Andrei <a.igonen>
2023-05-22 10:40:20 +0300

    Ititial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+function ListGroup() {
+  let items = ["London", "Paris", "Tokyo", "Helsinki"];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
